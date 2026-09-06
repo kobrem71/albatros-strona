acbc9e6a0a790bcf279dff36e2b6915b4d346fa3
kobrem <krzysztof.obremski@tensquaregames.com>
2026-09-06 23:20:53 +0200

    Trening bramkarski: poniedzialek 7.09 o 18:00

--- js/schedule.js
+++ js/schedule.js
@@ -50,6 +50,12 @@ export const EXTRA_EVENTS = [
     time: "16:00",
     location: HOME_ADDRESS,
   },
+  {
+    type: "trening-bramkarski",
+    date: "2026-09-07", // poniedziałek
+    time: "18:00",
+    location: HOME_ADDRESS,
+  },
 
   // Kolejka 3
   {
